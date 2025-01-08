bcf14ad77721fbe6a267fc78e6c5053c40809c6e
kanscerr <thisiskhushi2701@gmail.com>
2025-01-08 14:02:36 +0530

    email verification

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,19 +1,32 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
+    username: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String
+    password: {
+        type: String,
+        required: true
     },
-},{timestamps:true})
+    isVerified: {
+        type: Boolean,
+        default: false 
+    },
+    verificationCode: {
+        type: String,
+        required: false
+    },
+    verificationExpires: {
+        type: Date,
+        required: false
+    }
+}, { timestamps: true }); 
 
 const User = mongoose.model("user", userSchema);
 export default User;
